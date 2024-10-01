86d1ffc4ee7a6e313bde5b5a1e9f7135da42bb5c
navidm <navidm@zillowgroup.com>
2024-10-01 11:26:20 -0400

    chat scripts

---
+++ evaluation/chat.py
@@ -0,0 +1,105 @@
+import gradio as gr
+from transformers import AutoModelForCausalLM, AutoTokenizer, AutoConfig, LlamaForCausalLM, TextIteratorStreamer
+from fire import Fire
+from typing import List, Tuple
+import torch
+from transformers import BitsAndBytesConfig
+from data_generation.prompts import real_estate_chatbot_system_prompt
+
+def load_test_llama3_model(model_id):
+    config = AutoConfig.from_pretrained(model_id)
+    config.hidden_size = 256
+    config.intermediate_size = 16
+    config.num_hidden_layers = 2
+    config.num_attention_heads = 32
+    config.num_key_value_heads = 8
+    model = LlamaForCausalLM(config=config)
+    return model
+
+
+# system_prompt = real_estate_chatbot_system_prompt
+system_prompt = real_estate_chatbot_system_prompt
+
+#todo: add stremer to generation
+def run(model_name_or_path='Qwen/Qwen1.5-0.5B-Chat', local_test=False, max_new_tokens=512,
+        temperature=0.1, top_p=0.9, num_return_sequences=1, do_sample=True, num_beams=1,
+        qlora=False, share=False):
+
+    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+
+    # Load model and tokenizer
+    if local_test:
+        model = load_test_llama3_model(model_name_or_path)
+    elif qlora:
+        print("Using QLora model")
+        torch_dtype = torch.bfloat16
+        quant_storage_dtype = torch.bfloat16
+        bnb_4bit_use_double_quant = True
+        quantization_config = BitsAndBytesConfig(
+            load_in_4bit=True,
+            bnb_4bit_use_double_quant=bnb_4bit_use_double_quant,
+            bnb_4bit_quant_type="nf4",
+            bnb_4bit_compute_dtype=torch_dtype,
+            bnb_4bit_quant_storage=quant_storage_dtype,
+        )
+        model = AutoModelForCausalLM.from_pretrained(
+            model_name_or_path,
+            quantization_config=quantization_config,
+            torch_dtype=quant_storage_dtype,
+        )
+    else:
+        model = AutoModelForCausalLM.from_pretrained(model_name_or_path)
+        model = model.to(device)
+        model.eval()
+
+    tokenizer = AutoTokenizer.from_pretrained(model_name_or_path)
+
+    def build_input_from_chat_history(chat_history: List[Tuple], msg: str):
+        messages = [{'role': 'system', 'content': system_prompt}]
+        for user_msg, ai_msg in chat_history:
+            messages.append({'role': 'user', 'content': user_msg})
+            messages.append({'role': 'assistant', 'content': ai_msg})
+        messages.append({'role': 'user', 'content': msg})
+        return messages
+
+    # Define the chat function
+    def chat(message, chat_history):
+
+        messages = build_input_from_chat_history(chat_history, message)
+
+        input_ids = tokenizer.apply_chat_template(
+            messages,
+            add_generation_prompt=True,
+            return_tensors="pt",
+            tokenize=True
+        )
+
+        input_ids = input_ids.to(device)
+        output = model.generate(input_ids, max_new_tokens=max_new_tokens, pad_token_id=tokenizer.eos_token_id,
+                                do_sample=do_sample, temperature=temperature, top_p=top_p,
+                                num_return_sequences=num_return_sequences, num_beams=num_beams)
+
+        response_token_ids = output[0][input_ids.shape[1]:]
+        response = tokenizer.decode(response_token_ids, skip_special_tokens=True)
+        chat_history.append((message, response))
+
+        yield "", chat_history
+
+
+    iface = gr.Blocks()
+
+    with iface:
+        gr.Markdown("# Let's Chat!")
+        chatbox = gr.Chatbot([], layout="bubble", bubble_full_width=False)
+        msg = gr.Textbox(label="Your message")
+        clear = gr.Button("Clear")
+
+        msg.submit(chat, [msg, chatbox], [msg, chatbox])
+        clear.click(lambda: None, None, chatbox, queue=False)
+
+    # Launch the app
+    iface.launch(share=share)
+
+
+if __name__ == '__main__':
+    Fire(run)
---
+++ evaluation/live_chat_comparison.py
@@ -0,0 +1,92 @@
+import gradio as gr
+from transformers import AutoModelForCausalLM, AutoTokenizer, AutoConfig, LlamaForCausalLM, TextIteratorStreamer
+from fire import Fire
+from typing import List, Tuple
+import torch
+from transformers import BitsAndBytesConfig
+from data_generation.prompts import real_estate_chatbot_system_prompt
+from langchain_aws import ChatBedrock
+
+system_prompt = real_estate_chatbot_system_prompt
+
+
+# todo: add streamer to generation
+def run(model_name_or_path_1='Qwen/Qwen1.5-0.5B-Chat', model_name_or_path_2='meta.llama3-8b-instruct-v1:0',
+        max_new_tokens=1024,
+        temperature=0.7, top_p=0.9, num_return_sequences=1, do_sample=True, num_beams=1, share=False):
+
+    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+
+
+    model_1 = AutoModelForCausalLM.from_pretrained(model_name_or_path_1)
+    model_1 = model_1.to(device)
+    model_1.eval()
+    tokenizer_1 = AutoTokenizer.from_pretrained(model_name_or_path_1)
+
+    # Load model and tokenizer for the second model
+    model_2 = ChatBedrock(
+        model_id=model_name_or_path_2,
+        model_kwargs={"temperature": temperature, "max_gen_len": max_new_tokens},
+    )
+
+    def build_input_from_chat_history(chat_history: List[Tuple], msg: str):
+        messages = [{'role': 'system', 'content': system_prompt}]
+        for user_msg, ai_msg in chat_history:
+            messages.append({'role': 'user', 'content': user_msg})
+            messages.append({'role': 'assistant', 'content': ai_msg})
+        messages.append({'role': 'user', 'content': msg})
+        return messages
+
+    # Define the chat function for model 1
+    def chat_model_1(message, chat_history):
+        messages = build_input_from_chat_history(chat_history, message)
+        input_ids = tokenizer_1.apply_chat_template(
+            messages,
+            add_generation_prompt=True,
+            return_tensors="pt",
+            tokenize=True
+        )
+        input_ids = input_ids.to(device)
+        output = model_1.generate(input_ids, max_new_tokens=max_new_tokens, pad_token_id=tokenizer_1.eos_token_id,
+                                  do_sample=do_sample, temperature=temperature, top_p=top_p,
+                                  num_return_sequences=num_return_sequences, num_beams=num_beams)
+        response_token_ids = output[0][input_ids.shape[1]:]
+        response = tokenizer_1.decode(response_token_ids, skip_special_tokens=True)
+        chat_history.append((message, response))
+        return "", chat_history
+
+    # Define the chat function for model 2
+    def chat_model_2(message, chat_history):
+        messages = build_input_from_chat_history(chat_history, message)
+
+        response = model_2.invoke(messages).content
+        chat_history.append((message, response))
+        return "", chat_history
+
+    iface = gr.Blocks()
+
+    with iface:
+        gr.Markdown("# Compare Two Models Side by Side")
+        with gr.Row():
+            with gr.Column():
+                gr.Markdown(f"## {model_name_or_path_1.split('/')[-1]}")
+                chatbox_1 = gr.Chatbot([], layout="bubble", bubble_full_width=False)
+
+            with gr.Column():
+                gr.Markdown(f"## {model_name_or_path_2.split('/')[-1]}")
+                chatbox_2 = gr.Chatbot([], layout="bubble", bubble_full_width=False)
+
+        with gr.Row():
+            msg = gr.Textbox(label="Your message here")
+            msg.submit(chat_model_1, [msg, chatbox_1], [msg, chatbox_1])
+            msg.submit(chat_model_2, [msg, chatbox_2], [msg, chatbox_2])
+            clear = gr.Button("Clear Chat")
+            clear.click(lambda: None, None, chatbox_1, queue=False)
+            clear.click(lambda: None, None, chatbox_2, queue=False)
+
+    # Launch the app
+    iface.launch(share=share)
+
+
+if __name__ == '__main__':
+    Fire(run)
